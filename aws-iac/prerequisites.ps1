91e623e3e4de8430c5e2e831085068b8d70f4f12
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2021-05-24 11:11:18 +0300

    initial prerequsites

---
+++ aws-iac/prerequisites.ps1
@@ -0,0 +1,13 @@
+# 0. Install package manager
+#    Chocolatey is an open-source package manger for Windows
+#    See https://chocolatey.org/
+Set-ExecutionPolicy Bypass `
+    -Scope Process `
+    -Force;
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install terraform --version 0.15.3
+
+choco install terraformer --version 0.8.13
